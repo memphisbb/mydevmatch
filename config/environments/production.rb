1965328c399c7c7ef404b3aaf0213d7a82c515a7
Brad Brown <memphis.brad.brown@gmail.com>
2015-12-27 06:07:38 +0000

    bb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['bb-demo-dev'],
+    :bucket => ENV['bb-demo-production'],
     :access_key_id => ENV['AKIAITUVRQZEPPDOG3PQ'],
     :secret_access_key => ENV['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']
   }
