80a23ed5e962bdaf695a3473d9e13515b8a5407e
Brad Brown <memphis.brad.brown@gmail.com>
2015-12-28 16:42:47 +0000

    bb

--- config/initializers/paperclip.rb
+++
@@ -1,2 +0,0 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 'bb-demo-production.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
