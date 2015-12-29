026cf1bb30c0287110e111745dd3be4b6165b664
Brad Brown <memphis.brad.brown@gmail.com>
2015-12-29 02:11:13 +0000

    no

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,12 +36,14 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
 
-  config.paperclip_defaults = {
+
+config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'bb-demo-production',
-      :s3_credentials => "#{Rails.root}/config/application.yml",
+      :bucket => 'bb-demo-production', 
+      :access_key_id => ENV['AWS_ACCESS_KEY'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_region=> 'us-west-2',
     }
   }
-  
 end
